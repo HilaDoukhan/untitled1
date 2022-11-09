656881febbdadcb1d48ede637a57ab75b1b58ce6
Hila <hila@gmail.com>
2022-11-09 16:15:49 +0200

    this is some big change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is Hila's project");
+        System.out.println("This is Hila's awesome project");
+        System.out.println("Bye");
     }
 }
